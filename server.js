48452f329b33e200bc96856d487a03da61eb464b
Kaye Falaminiano <kayefalaminiano@gmail.com>
2026-05-06 18:33:43 -0700

    Adding redirect

--- server.js
+++ server.js
@@ -103,6 +103,30 @@ app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/about", (_req, res) => {
+  res.sendFile("about.html", {
+    root: "public/html",
+  });
+});
+
+app.get("/order", (_req, res) => {
+  res.sendFile("order.html", {
+    root: "public/html",
+  });
+});
+
+app.get("/contact", (_req, res) => {
+  res.sendFile("contact.html", {
+    root: "public/html",
+  });
+});
+
+app.get("/privacy", (_req, res) => {
+  res.sendFile("privacy.html", {
+    root: "public/html",
+  });
+});
+
 app.get("/", (_req, res) => {
   res.send("Emkari backend is running.");
 });
